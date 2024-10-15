a75805fba897847c59103772532ac46471e04d59
Kirill <fedorovkirillwww@yandex.ru>
2024-10-15 23:33:00 +0300

    Refactor iop.py: Add methods to manipulate event log

--- iop.py
+++ iop.py
@@ -12,3 +12,10 @@ class InputOutputProcessor:
         with open(json_file, 'w') as f:
             json.dump(data, f, indent=4)
     
+    def addRecordToEventLog(self, record: str):
+        with open('./data/event.log', 'a') as f:
+            f.write(f'{record}\n\n')
+    
+    def readEventLog(self):
+        with open('./data/event.log', 'r') as f:
+            return f.read()
