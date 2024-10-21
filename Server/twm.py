efacd7bd0e81629f0de0dda04ce9ab2f13f6911d
Kirill <fedorovkirillwww@yandex.ru>
2024-10-22 00:01:07 +0300

    Refactor email retrieval logic to handle empty email list

--- Server/twm.py
+++ Server/twm.py
@@ -123,6 +123,8 @@ class TellWithMeCommunicator(
         _, data = mail.search(None, "ALL")
         mail_ids = data[0]
         id_list = mail_ids.split()
+        if not id_list:
+            return None 
         latest_email_id = id_list[-1]
         _, data = mail.fetch(latest_email_id, "(RFC822)")
         raw_email = data[0][1]
