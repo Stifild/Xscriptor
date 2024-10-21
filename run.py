510abd518a1a2da7fda643b93f2e8e48bf01b62a
Kirill <fedorovkirillwww@yandex.ru>
2024-10-21 23:11:15 +0300

    Refactor run.py to include MAIN_USER_ID and bot message sending

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from configs import ROLE
+from configs import ROLE, MAIN_USER_ID
+from Server.main import bot
 from Server.twm import TellWithMe as SeTWM
 from Subord.twm import TellWithMe as SuTWM
-import threading, os, subprocess, socket
+import threading, os, subprocess, socket, platform
 
 if ROLE == "Server":
     twm = SeTWM()
@@ -9,11 +10,13 @@ if ROLE == "Server":
     checkMail = threading.Timer(3.5, twm.receive())
     while True:
         receive, flag = checkMail.start()
+        bot.send_message(MAIN_USER_ID, f"Flag:{flag}, Receive:{str(receive)}")
+        
         
          
 else:
     twm = SuTWM()
-    twm.send(twm.compileCommand("IND", socket.gethostname()), "0.0.0", "IND", "Hi Server! Can you give me an address?")
+    twm.send(twm.compileCommand("IND", {"name": socket.gethostname(), "os": platform.platform()}), "0.0.0", "IND", "Hi Server! Can you give me an address?")
     checkMail = threading.Timer(3.5, twm.receive())
     while True:
         receive, flag = checkMail.start()
